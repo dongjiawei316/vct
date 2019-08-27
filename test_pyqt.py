9de28a788f33d833a755d330bc2a986a750cf7d9
dongjw <dongjiawei@dvt.dvt.com>
2019-08-27 14:46:41 +0800

    从网上抄了一个例子，能够用opencv播放ts视频，调用了resize函数，使得视频能够全屏播放

---
+++ test_pyqt.py
@@ -0,0 +1,39 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial
+
+In this example, we create a simple
+window in PyQt5.
+
+author: Jan Bodnar
+website: zetcode.com
+Last edited: August 2017
+"""
+
+import os, sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('loss_rate.png'))
+
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    os.system(r"ffplay.exe 1.ts -x 800 -y 600 ")
+
+    sys.exit(app.exec_())
