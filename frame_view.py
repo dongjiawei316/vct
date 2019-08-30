0d24f05f3436de2fbade13ba76a6a09c1872dd20
dongjw <dongjiawei@dvt.dvt.com>
2019-08-30 10:35:01 +0800

    能够通过ffmpeg解码，然后经过cv转化，最后显示在QT界面上

--- frame_view.py
+++ frame_view.py
@@ -1,37 +1,75 @@
 # -*- coding: utf-8 -*-
-
+import sys
 from ipywidgets import interact
 from matplotlib import pyplot as plt
 import ffmpeg
 import ipywidgets as widgets
 import numpy as np
+import time
+from PIL import Image
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from cv2 import *
+import _thread as thread
 
-probe = ffmpeg.probe('resource/in.mp4')
+probe = ffmpeg.probe('resource/1.ts')
 video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
 width = int(video_info['width'])
 height = int(video_info['height'])
-num_frames = int(video_info['nb_frames'])
+#num_frames = int(video_info['nb_frames'])
 
-out, err = (
+mapp = QApplication(sys.argv)
+win = QWidget()
+pictureLabel = QLabel()
+init_image = QPixmap("resource/cat.jpeg").scaled(640, 480)
+pictureLabel.setPixmap(init_image)
+
+layout = QVBoxLayout()
+layout.addWidget(pictureLabel)
+
+win.setLayout(layout)
+win.show()
+print("QWidget ok!")
+
+
+process1 = (
     ffmpeg
-    .input('resource/in.mp4')
+    .input("resource/1.ts")
     .output('pipe:', format='rawvideo', pix_fmt='rgb24')
-    .run(capture_stdout=True)
-)
-video = (
-    np
-    .frombuffer(out, np.uint8)
-    .reshape([-1, height, width, 3])
+    .run_async(pipe_stdout=True)
 )
 
-print(str(width) + " " + str(height) + " " + str(num_frames) )
+def extract_frame(process1):
+    while True:
 
-@interact(frame=(0, num_frames))
-def show_frame(frame=0):
-    plt.imshow(video[frame,:,:,:])
+        in_bytes = process1.stdout.read(width * height * 3)
+        if not in_bytes:
+            break
+
+        in_frame = (
+            np
+                .frombuffer(in_bytes, np.uint8)
+                .reshape([height, width,3])
+        )
+
+        frame = cv2.resize(in_frame, (640, 480))
+        temp_image = QImage(frame, 640, 480, QImage.Format_RGB888)
+        temp_pixmap = QPixmap.fromImage(temp_image)
+        pictureLabel.setPixmap(temp_pixmap)
+        time.sleep(0.04)
+
+
+thread.start_new_thread( extract_frame, (process1, ) )
+
+sys.exit(mapp.exec_())
+
+process1.wait()
 
 
 
+
+"""
 from io import BytesIO
 from PIL import Image
 
@@ -120,3 +158,4 @@ def f(
     plt.tight_layout()
     show_image(ax0, stream, frame_num)
     show_graph(ax1, stream, graph_detail)
+"""
